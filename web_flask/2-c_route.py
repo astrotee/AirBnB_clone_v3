958ecc087d6ebac3f008e8ea5ad50561296715c3
amin ahmed <aminahmed24@gmail.com>
2024-07-26 04:02:40 +0300

    2. C is fun!

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""hello, world with flask"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """root route"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """hbnb route"""
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c(text: str):
+    """what about C?"""
+    return f"C {text.replace('_', ' ')}"
+
+
+if __name__ == "__main__":
+    app.run('0.0.0.0', 5000)
