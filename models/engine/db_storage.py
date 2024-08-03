7e8416c9e4679a5d715141cc7faf1ed5cd02d659
amin ahmed <aminahmed24@gmail.com>
2024-08-03 19:35:35 +0300

    6. DBStorage - States and Cities

---
+++ models/engine/db_storage.py
@@ -0,0 +1,70 @@
+#!/usr/bin/python3
+"""This module defines a class to manage database storage for hbnb clone"""
+import os
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+from models.amenity import Amenity
+from models.base_model import Base, BaseModel
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+
+
+class DBStorage:
+    """This class manages storage of hbnb models in database"""
+    __engine = None
+    __session = None
+
+    def __init__(self) -> None:
+        user = os.getenv("HBNB_MYSQL_USER", "hbnb_dev")
+        password = os.getenv("HBNB_MYSQL_PWD", "hbnb_dev_pwd")
+        host = os.getenv("HBNB_MYSQL_HOST", "localhost")
+        database = os.getenv("HBNB_MYSQL_DB", "hbnb_dev_db")
+        environment = os.getenv("HBNB_ENV", "dev")
+        self.__engine = create_engine(
+                f"mysql+mysqldb://{user}:{password}@{host}/{database}",
+                pool_pre_ping=True)
+        if environment == "test":
+            Base.metadata.drop_all()
+
+    def all(self, cls=None):
+        """Returns a dictionary of models currently in storage"""
+        records = dict()
+        tables: list
+
+        classes = {
+                    'BaseModel': BaseModel, 'User': User, 'Place': Place,
+                    'State': State, 'City': City, 'Amenity': Amenity,
+                    'Review': Review
+                  }
+        if cls:
+            tables = [cls]
+        else:
+            tables = [State, City]
+        for c in tables:
+            objs = self.__session.query(c).all()
+            records.update({f"{o.__class__.__name__}.{o.id}": o for o in objs})
+        return records
+
+    def new(self, obj):
+        """Adds new object to storage dictionary"""
+        self.__session.add(obj)
+
+    def delete(self, obj=None):
+        """Deletes an object from storage dictionary"""
+        if obj:
+            self.__session.delete(obj)
+
+    def save(self):
+        """Saves storage dictionary to file"""
+        self.__session.commit()
+
+    def reload(self):
+        """Loads storage dictionary from file"""
+        Base.metadata.create_all(self.__engine)
+        session_factory = sessionmaker(bind=self.__engine,
+                                       expire_on_commit=False)
+        self.__session = scoped_session(session_factory)()
--- models/state.py
+++ models/state.py
@@ -1,8 +1,25 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from models.base_model import Base, BaseModel
+from models.city import City
+from models import storage_type
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    __tablename__ = "states"
+    if storage_type == "db":
+        name = Column(String(128), nullable=False)
+        cities = relationship("City", backref="state",
+                              cascade="all, delete, delete-orphan")
+    else:
+        name = ''
+
+        @property
+        def cities(self):
+            """get cities that belong to that state"""
+            from models import storage
+            cs = storage.all(City)
+            return [c for c in cs if cs.state_id == self.id]
